57364bd4008ee3f421fd93cd7a5e29dba25c7063
Ivan Baklanov <ibaklanov99@gmail.com>
2022-10-14 16:33:31 +0300

    added secured access to home and logout pages based on user login status

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from os import path
 
 db = SQLAlchemy()
@@ -13,6 +14,8 @@ def creat_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
+    
+    
 
     from .auth import auth
     from .views import views
@@ -22,6 +25,14 @@ def creat_app():
     from .models import User, Note
 
     create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
